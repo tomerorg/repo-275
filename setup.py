10c27164b057506afa527a58571b4a60e816ab5a
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:23:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-275",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Python iot 275 with FastAPI and Django",
+    keywords="repo-275, python",
+    url="",
+)
